ed2d6f480f2005788d6600d99c63677ba89ed0b9
gauthamdasu <gauthamdasu@gmail.com>
2020-12-12 23:13:58 +0530

    time basic structure and linting

--- modules.py
+++ modules.py
@@ -1,10 +1,15 @@
 import schedule
 import webbrowser
+import datetime
 
 
 def open_mail():
     url = "https://mail.google.com"
     webbrowser.open(url)
+
+
+def read_time():
+    return datetime.datetime.now().time()	
 
 
 if __name__=="__main__":
--- voias.py
+++ voias.py
@@ -7,40 +7,57 @@ import speech_recognition as sr
 import os
 import time
 import schedule
-from modules import open_mail
+from modules import open_mail, read_time
 
-
+# Initi. the speech recognition module
 r = sr.Recognizer()
+
+# Give a welcome message
 os.system("espeak ' Hello BOSS. I am voias, I am your simple personal assistant, how can i help you' - s 60")		    
+
+# First while loop for triggering our assistant
 while True:
+	# Init, the microphone setting for listening the voice input from the user.
+
 	with sr.Microphone(device_index = 0, sample_rate = 48000, chunk_size = 1024) as source1:
 		r.adjust_for_ambient_noise(source1)
 		audio = r.listen(source1)
 		print("Listened your command, please wait i'm processing it")	
+
 	try:
 		trigger_word = r.recognize_google(audio)
 		print(trigger_word)
+
+		#check for the trigger word.
 		if trigger_word == "okay" or trigger_word == "ok":
 			print(trigger_word)
 			print("Triggering VOIAS")
+
 		if trigger_word == "buy" or trigger_word== "bye" or trigger_word== "end" :
 			os.system("espeak 'Any time at your service' - s 60")
 			os.system("espeak 'have a good day' - s 60")
 			break
+
+
+		# Second loop for getting the command form the user.
+
 		while trigger_word=="okay" or trigger_word == "ok":
 			print("Please give your command")
 			os.system("espeak 'triggered Voias, please give your command.' - s 60")		
 			ir = sr.Recognizer()
-			with sr.Microphone(device_index = 0, sample_rate = 48000, chunk_size = 2048) as source:
+			with sr.Microphone(device_index = 0, sample_rate = 48000, chunk_size = 1024) as source:
 				ir.adjust_for_ambient_noise(source)		
 				audio = ir.listen(source)
 				print ("Listened, please wait I'm processing it......")
-			res=ir.recognize_google(audio)
+			res = ir.recognize_google(audio)
 			print(res)
 			os.system("espeak 'Listened please wait i'm processing it' - s 60")
 			if res == "mail":
 				os.system("espeak 'Opening the mail BOSS'")
 				open_mail()
+			if res == "time":
+				message = "espeak " + "'"+"time is "+str(read_time()) +"'"
+				os.system(message)
 			if res=="thank you":
 				os.system("espeak 'Thanks for using'")
 				break
